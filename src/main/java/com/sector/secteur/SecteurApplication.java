106bddbd7341b23ea8285cf2b2ba441953d5fa98
StefanGucu <stefan.gucu@gmx.com>
2021-06-17 19:16:03 +0200

    This is a initial commit

---
+++ src/main/java/com/sector/secteur/SecteurApplication.java
@@ -0,0 +1,13 @@
+package com.sector.secteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecteurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sector/secteur/SecteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sector.secteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
